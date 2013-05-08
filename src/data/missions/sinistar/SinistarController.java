34f097ae5bebffad35c75905b644550893a57af5
Rob Maguire <iu65a@hotmail.com>
2013-05-08 01:47:16 -0500

    Sinistar now hovers near his target instead of ramming them (needs weapons!)

--- src/data/missions/sinistar/SinistarController.java
+++ src/data/missions/sinistar/SinistarController.java
@@ -2,6 +2,7 @@ package data.missions.sinistar;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.CombatEntityAPI;
 import com.fs.starfarer.api.combat.EveryFrameCombatPlugin;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
@@ -14,14 +15,22 @@ import org.lwjgl.util.vector.Vector2f;
 
 public class SinistarController implements EveryFrameCombatPlugin
 {
-    private static final float BITE_DAMAGE = 500f; // Bite damage per second
-    private static final int NUM_TAUNTS = 5; // How many taunts are in sounds.json
-    private CombatEngineAPI engine; // The CombatEngineAPI used by this battle
-    private FleetMemberAPI sinistarFleetMember; // Sinistar's fleet member object
-    private ShipAPI sinistar; // Sinistar's in-battle ship object
-    private float actualFacing = 0; // Sinistar's manually calculated facing
-    private boolean hasWarned = false, hasDied = false; // One-time only events
-    private IntervalUtil nextTaunt = new IntervalUtil(7.5f, 15f); // Time between taunts
+    // How far away to hover in front of our current attack target
+    private static final float OPTIMAL_DISTANCE_FROM_TARGET = 150f;
+    // How many taunts are in sounds.json
+    private static final int NUM_TAUNTS = 5;
+    // The combat engine object used by this battle
+    private CombatEngineAPI engine;
+    // Sinistar's pre-battle fleet member object
+    private FleetMemberAPI sinistarFleetMember;
+    // Sinistar's in-battle ship object
+    private ShipAPI sinistar;
+    // Sinistar's manually calculated facing
+    private float actualFacing = 0;
+    // Whether one-time events (spawn, death) have been triggered yet
+    private boolean hasSpawned = false, hasDied = false;
+    // Time between taunts
+    private IntervalUtil nextTaunt = new IntervalUtil(7.5f, 15f);
 
     public SinistarController(FleetMemberAPI sinistarFleetMember)
     {
@@ -32,7 +41,7 @@ public class SinistarController implements EveryFrameCombatPlugin
 
     private void checkSpawn()
     {
-        // Find Sinistar
+        // Find the ShipAPI that matches the FleetMemberAPI passed into the constructor
         sinistar = engine.getFleetManager(FleetSide.ENEMY).getShipFor(sinistarFleetMember);
 
         // For now, deliberately crash the game if Sinistar isn't found
@@ -44,9 +53,17 @@ public class SinistarController implements EveryFrameCombatPlugin
             throw new RuntimeException("Sinistar not found!");
         }
 
+        // Give Sinistar some special properties
+        MutableShipStatsAPI stats = sinistar.getMutableStats();
+        stats.getSightRadiusMod().modifyMult("sinistar", 2.0f);
+        stats.getEngineDamageTakenMult().modifyMult("sinistar", 0.001f);
+        stats.getWeaponDamageTakenMult().modifyMult("sinistar", 0.001f);
+        stats.getCombatEngineRepairTimeMult().modifyMult("sinistar", 9999f);
+        stats.getCombatWeaponRepairTimeMult().modifyMult("sinistar", 9999f);
+
         // Play Sinistar's introductory taunt
         Global.getSoundPlayer().playUISound("sinistar_spawn", 1f, 1f);
-        hasWarned = true;
+        hasSpawned = true;
     }
 
     private void checkTaunt(float amount)
@@ -72,19 +89,29 @@ public class SinistarController implements EveryFrameCombatPlugin
 
     private void doMovement(float amount)
     {
-        // Pick the target we will be attacking (ramming)
-        Vector2f target;
-        if (sinistar.getShipTarget() != null)
+        // The location Sinistar will choose to travel towards
+        Vector2f destination;
+
+        // Prefer the AI-chosen target, use player ship if all else fails
+        CombatEntityAPI target = sinistar.getShipTarget();
+        if (target == null)
         {
-            target = sinistar.getShipTarget().getLocation();
+            // Sinistar ALWAYS knows where you are :O
+            target = engine.getPlayerShip();
         }
-        else if (engine.getPlayerShip() != null)
+
+        if (target != null)
         {
-            target = engine.getPlayerShip().getLocation();
+            // Try to stay a certain distance away from the target
+            destination = MathUtils.getPointOnCircumference(target.getLocation(),
+                    target.getCollisionRadius() + sinistar.getCollisionRadius()
+                    + OPTIMAL_DISTANCE_FROM_TARGET, MathUtils.getAngle(
+                    target.getLocation(), sinistar.getLocation()));
         }
         else
         {
-            target = new Vector2f(0, 0);
+            // No targets available? Head to the center of the map and wait
+            destination = new Vector2f(0, 0);
         }
 
         // Get mobility values from Sinistar's MutableShipStats
@@ -94,7 +121,7 @@ public class SinistarController implements EveryFrameCombatPlugin
         float turnRate = stats.getMaxTurnRate().getModifiedValue();
 
         // Calculate and set actual facing and velocity manually
-        float intendedFacing = MathUtils.getAngle(sinistar.getLocation(), target);
+        float intendedFacing = MathUtils.getAngle(sinistar.getLocation(), destination);
         float facingChange = (intendedFacing - actualFacing) * amount * turnRate;
         float intendedSpeed = Math.min(sinistar.getVelocity().length()
                 + (accel * amount), maxSpeed);
@@ -120,7 +147,7 @@ public class SinistarController implements EveryFrameCombatPlugin
         }
 
         // One time setup at battle start
-        if (!hasWarned)
+        if (!hasSpawned)
         {
             checkSpawn();
         }
